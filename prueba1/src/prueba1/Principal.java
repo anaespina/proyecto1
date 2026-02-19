228a530319ecdea75bdacb01f1ae0ad4c5a0a877
Usu2 Name <usu2@example.com>
2026-02-19 12:40:24 +0100

    Añado una línea en la clase Principal.

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 	}
 
 }
